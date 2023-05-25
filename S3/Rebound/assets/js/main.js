54109416668e91c2d4c9557477e3270f65ed2452
Genesis Voisier <voassie@gmail.com>
2023-05-25 16:33:08 -0400

    Sobre tres ingredientes costo adicional

--- S3/Rebound/assets/js/main.js
+++ S3/Rebound/assets/js/main.js
@@ -1,8 +1,6 @@
-//  []Los clientes pueden seleccionar 3 ingredientes gratis.
-//      []Obtener los valores de los input de los ingredientes seleccionados por el cliente.
+//  [x]Los clientes pueden seleccionar 3 ingredientes gratis.
 //      []Los ingredientes seleccionados gratis se deben mostrar en la pantalla en: <p id="ingSeleccionados">
-//  []Sobre 3 ingredientes, el cliente debe pagar $800 c/u
-//      []If/of para que itere sobre los ingredientes, si hay más se tres, cobrar 800 c/u
+//  [x]Sobre 3 ingredientes, el cliente debe pagar $800 c/u
 //  []Al seleccionar los ingredientes gratis se debe proyectar en la ventana derecha el valor de la pizza XL (15.000)
 //  []Los ingredientes seleccionados EXTRA deben mostrarse en pantalla en: <p id="ingExtras">
 //  []Los ingredientes seleccionados EXTRA deben mostrarse en la ventana derecha como "ingredientes extra" y su valor
@@ -11,3 +9,26 @@
 //  []Al hacer click en "enviar pedido con propina", se debe enviar un mensaje por medio de un alert: "Su propina de $2.500 ha sido enviada"
 //  []Si el pedido no tiene propina, el mensaje de alerta debe mostrar: "Aún no se ha definido una propina"
 
+const checkboxes = document.querySelectorAll('input[name="ingrediente"]');
+const maximoGratis = 3; // Máximo número de checkboxes permitidos
+let checkContador = 0; // Contador de checkboxes seleccionados
+const costoBase = 15000; // Monto base, valor pizza XL
+
+// Añadir un controlador de eventos a cada checkbox
+checkboxes.forEach(function(checkbox) {
+  checkbox.addEventListener('change', function() {
+    // Actualizar el contador según el estado del checkbox
+    if (this.checked) {
+        checkContador++;
+    } else {
+        checkContador--;
+    }
+    
+    // Verificar si se ha excedido el límite
+    if (checkContador > maximoGratis) {
+        let ingredientesExtra = Math.max(0, checkContador - maximoGratis) * 800;
+        let sumarMontoExtra = costoBase + ingredientesExtra;
+        console.log(sumarMontoExtra);
+    }
+  });
+});
