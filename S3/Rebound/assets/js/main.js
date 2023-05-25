5d8dcee267c436695a8d83440e674d9235cd1f5b
Genesis Voisier <voassie@gmail.com>
2023-05-25 17:20:13 -0400

    Ventana izquierda funcionando

--- S3/Rebound/assets/js/main.js
+++ S3/Rebound/assets/js/main.js
@@ -1,12 +1,12 @@
 /*  [x]Los clientes pueden seleccionar 3 ingredientes gratis.
-      []Los ingredientes seleccionados gratis se deben mostrar en la pantalla en: <p id="ingSeleccionados">
+      [x]Los ingredientes seleccionados gratis se deben mostrar en la pantalla en: <p id="ingSeleccionados">
     [x]Sobre 3 ingredientes, el cliente debe pagar $800 c/u
       [x]querySelectorAll para obtener el valor de los input.
       [x]forEach que recorre cada checkbox seleccionado por el cliente para obtener el valor
       [x]Cada selección se sumará a la variable checkContador.
       [x]A > de 3 selecciones se contará como ingrediente extra
     []Al seleccionar los ingredientes gratis se debe proyectar en la ventana derecha el valor de la pizza XL (15.000)
-    []Los ingredientes seleccionados EXTRA deben mostrarse en pantalla en: <p id="ingExtras">
+    [x]Los ingredientes seleccionados EXTRA deben mostrarse en pantalla en: <p id="ingExtras">
     []Los ingredientes seleccionados EXTRA deben mostrarse en la ventana derecha como "ingredientes extra" y su valor
     []Al hacer click en "ingresar propina", se deben agregar $1.000 pesos como valor por defecto.
     []Al ingresar otro valor de propina, se debe ver reflejado el cambio en la pantalla.
