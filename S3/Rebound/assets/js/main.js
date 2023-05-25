c2a21e57cf881c755557a55bec3bd63e3c9cd803
Genesis Voisier <voassie@gmail.com>
2023-05-25 17:18:55 -0400

    JS de la ventana derecha funcionando

--- S3/Rebound/assets/js/main.js
+++ S3/Rebound/assets/js/main.js
@@ -1,34 +1,67 @@
-//  [x]Los clientes pueden seleccionar 3 ingredientes gratis.
-//      []Los ingredientes seleccionados gratis se deben mostrar en la pantalla en: <p id="ingSeleccionados">
-//  [x]Sobre 3 ingredientes, el cliente debe pagar $800 c/u
-//  []Al seleccionar los ingredientes gratis se debe proyectar en la ventana derecha el valor de la pizza XL (15.000)
-//  []Los ingredientes seleccionados EXTRA deben mostrarse en pantalla en: <p id="ingExtras">
-//  []Los ingredientes seleccionados EXTRA deben mostrarse en la ventana derecha como "ingredientes extra" y su valor
-//  []Al hacer click en "ingresar propina", se deben agregar $1.000 pesos como valor por defecto.
-//  []Al ingresar otro valor de propina, se debe ver reflejado el cambio en la pantalla.
-//  []Al hacer click en "enviar pedido con propina", se debe enviar un mensaje por medio de un alert: "Su propina de $2.500 ha sido enviada"
-//  []Si el pedido no tiene propina, el mensaje de alerta debe mostrar: "Aún no se ha definido una propina"
+/*  [x]Los clientes pueden seleccionar 3 ingredientes gratis.
+      []Los ingredientes seleccionados gratis se deben mostrar en la pantalla en: <p id="ingSeleccionados">
+    [x]Sobre 3 ingredientes, el cliente debe pagar $800 c/u
+      [x]querySelectorAll para obtener el valor de los input.
+      [x]forEach que recorre cada checkbox seleccionado por el cliente para obtener el valor
+      [x]Cada selección se sumará a la variable checkContador.
+      [x]A > de 3 selecciones se contará como ingrediente extra
+    []Al seleccionar los ingredientes gratis se debe proyectar en la ventana derecha el valor de la pizza XL (15.000)
+    []Los ingredientes seleccionados EXTRA deben mostrarse en pantalla en: <p id="ingExtras">
+    []Los ingredientes seleccionados EXTRA deben mostrarse en la ventana derecha como "ingredientes extra" y su valor
+    []Al hacer click en "ingresar propina", se deben agregar $1.000 pesos como valor por defecto.
+    []Al ingresar otro valor de propina, se debe ver reflejado el cambio en la pantalla.
+    []Al hacer click en "enviar pedido con propina", se debe enviar un mensaje por medio de un alert: "Su propina de $2.500 ha sido enviada"
+    []Si el pedido no tiene propina, el mensaje de alerta debe mostrar: "Aún no se ha definido una propina"
+*/
+
 
 const checkboxes = document.querySelectorAll('input[name="ingrediente"]');
 const maximoGratis = 3; // Máximo número de checkboxes permitidos
 let checkContador = 0; // Contador de checkboxes seleccionados
 const costoBase = 15000; // Monto base, valor pizza XL
+let ingSeleccionados = document.getElementById("ingSeleccionados");
 
-// Añadir un controlador de eventos a cada checkbox
+//For que recorre cada checkbox seleccionado por el cliente para obtener el valor
+//Cada selección se sumará a la variable checkContador
 checkboxes.forEach(function(checkbox) {
   checkbox.addEventListener('change', function() {
-    // Actualizar el contador según el estado del checkbox
+
     if (this.checked) {
         checkContador++;
     } else {
         checkContador--;
     }
     
-    // Verificar si se ha excedido el límite
+    //A > de 3 selecciones se contará como ingrediente extra
     if (checkContador > maximoGratis) {
         let ingredientesExtra = Math.max(0, checkContador - maximoGratis) * 800;
         let sumarMontoExtra = costoBase + ingredientesExtra;
         console.log(sumarMontoExtra);
     }
+
+    //Mostrar en pantalla derecha los tres ingredientes gratis.
+    function obtenerIngredientesSeleccionados() {
+        var ingredientes = [];
+        
+        checkboxes.forEach(function(checkbox) {
+          if (checkbox.checked) {
+            ingredientes.push(checkbox.value);
+          }
+        });
+        
+        return ingredientes;
+      }
+      //Mostrando los ingredientes gratis.
+      let ingredientesGratisSeleccionados = obtenerIngredientesSeleccionados().slice(0, 3);
+      document.getElementById("ingSeleccionados").innerHTML = ingredientesGratisSeleccionados;
+      //Mostrando los ingredientes extra.
+      let ingredientesExtrasSeleccionados = obtenerIngredientesSeleccionados().slice(3);
+      document.getElementById("ingExtras").innerHTML = ingredientesExtrasSeleccionados;
   });
 });
+
+
+
+
+
+// Math.max(0, checkContador - maximoGratis) se utiliza para calcular la cantidad de ingredientes adicionales más allá del límite establecido (maximoGratis) y luego se multiplica por 800 para obtener el costo adicional correspondiente.
