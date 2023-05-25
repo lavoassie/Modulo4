4827c98a49f46786da70de168dcb938a2e48d15f
Genesis Voisier <voassie@gmail.com>
2023-05-25 15:44:12 -0400

    Avanzando en las tareas por realizar

---
+++ S3/Rebound/assets/js/main.js
@@ -0,0 +1,13 @@
+//  []Los clientes pueden seleccionar 3 ingredientes gratis.
+//      []Obtener los valores de los input de los ingredientes seleccionados por el cliente.
+//      []Los ingredientes seleccionados gratis se deben mostrar en la pantalla en: <p id="ingSeleccionados">
+//  []Sobre 3 ingredientes, el cliente debe pagar $800 c/u
+//      []If/of para que itere sobre los ingredientes, si hay más se tres, cobrar 800 c/u
+//  []Al seleccionar los ingredientes gratis se debe proyectar en la ventana derecha el valor de la pizza XL (15.000)
+//  []Los ingredientes seleccionados EXTRA deben mostrarse en pantalla en: <p id="ingExtras">
+//  []Los ingredientes seleccionados EXTRA deben mostrarse en la ventana derecha como "ingredientes extra" y su valor
+//  []Al hacer click en "ingresar propina", se deben agregar $1.000 pesos como valor por defecto.
+//  []Al ingresar otro valor de propina, se debe ver reflejado el cambio en la pantalla.
+//  []Al hacer click en "enviar pedido con propina", se debe enviar un mensaje por medio de un alert: "Su propina de $2.500 ha sido enviada"
+//  []Si el pedido no tiene propina, el mensaje de alerta debe mostrar: "Aún no se ha definido una propina"
+
