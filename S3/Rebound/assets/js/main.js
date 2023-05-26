cd3f5e452cf5c0ced8fed7aa82264c6beb5dfcff
Genesis Voisier <voassie@gmail.com>
2023-05-26 10:18:24 -0400

    Muestra el valor de los ingredientes EXTRA

--- S3/Rebound/assets/js/main.js
+++ S3/Rebound/assets/js/main.js
@@ -7,7 +7,7 @@
       [x]A > de 3 selecciones se contará como ingrediente extra
     []Al seleccionar los ingredientes gratis se debe proyectar en la ventana derecha el valor de la pizza XL (15.000)
     [x]Los ingredientes seleccionados EXTRA deben mostrarse en pantalla en: <p id="ingExtras">
-    []Los ingredientes seleccionados EXTRA deben mostrarse en la ventana derecha como "ingredientes extra" y su valor
+    [x]El valor de los ingredientes seleccionados EXTRA deben mostrarse en la ventana derecha en "ingredientes extra" 
     []Al hacer click en "ingresar propina", se deben agregar $1.000 pesos como valor por defecto.
     []Al ingresar otro valor de propina, se debe ver reflejado el cambio en la pantalla.
     []Al hacer click en "enviar pedido con propina", se debe enviar un mensaje por medio de un alert: "Su propina de $2.500 ha sido enviada"
@@ -23,45 +23,44 @@ let ingSeleccionados = document.getElementById("ingSeleccionados");
 
 //For que recorre cada checkbox seleccionado por el cliente para obtener el valor
 //Cada selección se sumará a la variable checkContador
-checkboxes.forEach(function(checkbox) {
-  checkbox.addEventListener('change', function() {
+checkboxes.forEach(function (checkbox) {
+  checkbox.addEventListener('change', function () {
 
     if (this.checked) {
-        checkContador++;
+      checkContador++;
     } else {
-        checkContador--;
+      checkContador--;
     }
-    
+
     //A > de 3 selecciones se contará como ingrediente extra
     if (checkContador > maximoGratis) {
-        let ingredientesExtra = Math.max(0, checkContador - maximoGratis) * 800;
-        let sumarMontoExtra = costoBase + ingredientesExtra;
-        console.log(sumarMontoExtra);
+      let ingredientesExtra = Math.max(0, checkContador - maximoGratis) * 800;
+      let totalAPagar = costoBase + ingredientesExtra;
+      console.log(totalAPagar);
+      //El valor de los ingredientes seleccionados EXTRA deben mostrarse en la ventana derecha en "ingredientes extra"
+      document.getElementById("tdExtras").innerHTML = ingredientesExtra
     }
 
     //Mostrar en pantalla derecha los tres ingredientes gratis.
     function obtenerIngredientesSeleccionados() {
-        var ingredientes = [];
-        
-        checkboxes.forEach(function(checkbox) {
-          if (checkbox.checked) {
-            ingredientes.push(checkbox.value);
-          }
-        });
-        
-        return ingredientes;
-      }
-      //Mostrando los ingredientes gratis.
-      let ingredientesGratisSeleccionados = obtenerIngredientesSeleccionados().slice(0, 3);
-      document.getElementById("ingSeleccionados").innerHTML = ingredientesGratisSeleccionados;
-      //Mostrando los ingredientes extra.
-      let ingredientesExtrasSeleccionados = obtenerIngredientesSeleccionados().slice(3);
-      document.getElementById("ingExtras").innerHTML = ingredientesExtrasSeleccionados;
+      var ingredientes = [];
+
+      checkboxes.forEach(function (checkbox) {
+        if (checkbox.checked) {
+          ingredientes.push(checkbox.value);
+        }
+      });
+
+      return ingredientes;
+    }
+    //Mostrando los ingredientes gratis.
+    let ingredientesGratisSeleccionados = obtenerIngredientesSeleccionados().slice(0, 3);
+    document.getElementById("ingSeleccionados").innerHTML = ingredientesGratisSeleccionados;
+    //Agregar el valor de la pizza XL
+    //Mostrando los ingredientes extra.
+    let ingredientesExtrasSeleccionados = obtenerIngredientesSeleccionados().slice(3);
+    document.getElementById("ingExtras").innerHTML = ingredientesExtrasSeleccionados;
+
   });
 });
 
-
-
-
-
-// Math.max(0, checkContador - maximoGratis) se utiliza para calcular la cantidad de ingredientes adicionales más allá del límite establecido (maximoGratis) y luego se multiplica por 800 para obtener el costo adicional correspondiente.
