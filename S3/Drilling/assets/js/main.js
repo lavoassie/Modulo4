1870af5b77a1fc616526a389ac12b60e3fe7baee
Genesis Voisier <voassie@gmail.com>
2023-05-26 17:09:33 -0400

    En consola se ve:PARA,TITULO,MENSAJE,FROM

--- S3/Drilling/assets/js/main.js
+++ S3/Drilling/assets/js/main.js
@@ -1,4 +1,5 @@
-//  [x]Obtener los valores de inputs y botón.
+//  []Obtener los valores de inputs y botón.
+//      [x]guardar en una variable los inputs y botón.
 //  []Mostrar en consola: para, titulo, mensaje, from
 //  []Mosotrar en consola los emails CC
 //  []Incluir múltiples correos electrónicos como destinatarios de CC. 
@@ -8,6 +9,9 @@ let inputDestinatario = document.getElementById("inputDestinatario");
 let inputTitulo = document.getElementById("inputTitulo");
 let inputMensaje = document.getElementById("inputMensaje");
 let inputRemitente = document.getElementById("inputRemitente");
-let correosCC = document.getElementById("correosCC");
 let botonEnviar = document.getElementById("btnEnviar");
+let correos = [];
 
+botonEnviar.addEventListener("click", function(...ccCorreos) {
+  console.log(`PARA: ${inputDestinatario.value} \n TÍTULO: ${inputTitulo.value} \n MENSAJE: ${inputMensaje.value} \n FROM: ${inputRemitente.value}`);
+});
