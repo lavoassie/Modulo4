e734fdd045bdb6cf050b0f8377d003ef1191e92c
Genesis Voisier <voassie@gmail.com>
2023-05-26 16:35:15 -0400

    Obtener los valores de inputs y botón

---
+++ S3/Drilling/assets/js/main.js
@@ -0,0 +1,13 @@
+//  [x]Obtener los valores de inputs y botón.
+//  []Mostrar en consola: para, titulo, mensaje, from
+//  []Mosotrar en consola los emails CC
+//  []Incluir múltiples correos electrónicos como destinatarios de CC. 
+//  []Utilizando al menos un parámetro Rest y un bucle for of.
+
+let inputDestinatario = document.getElementById("inputDestinatario");
+let inputTitulo = document.getElementById("inputTitulo");
+let inputMensaje = document.getElementById("inputMensaje");
+let inputRemitente = document.getElementById("inputRemitente");
+let correosCC = document.getElementById("correosCC");
+let botonEnviar = document.getElementById("btnEnviar");
+
